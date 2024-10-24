2e6f9bdbb543405225d5ea00c9311032531fc746
Enileidy <Enileydi.ro@gmail.com>
2024-10-23 21:47:29 -0300

    Meu primeiro commit

---
+++ app/src/main/java/com/example/plyground/Main.kt
@@ -0,0 +1,31 @@
+package com.example.plyground
+
+fun main() {
+
+    Class Calculadora()
+
+
+
+        fun adicao (valor1: Int, valor2: Int): Int {
+            Val resultado = valor1 + valor2
+            return resultado
+    }
+
+}
+   fun subtracao (valor1: Int, valor2: Int): Int {
+       return valor1 - valor2
+
+   }
+
+    fun divisao (valor1: Double, valor2: Double): Double {
+    return valor1 / valor2
+}
+        fun main()
+        val Calculadora = calculadora(5.0,10.0)
+        
+        println(Calculadora.adicao(5,10))
+        println(Calculadora.subtracao(5,10))
+        println(Calculadora.divisao(5.0,10))
+        println(Calculadora.multiplicacao(5,10))
+
+}
